848c4f59ab18deb9328c02fb0d0a7162a582c745
Prudhvi Boyapalli <prrb02@gmail.com>
2017-02-21 23:45:29 -0600

    Created viz page and set up basic force graph with sample data

--- metapaths.py
+++ metapaths.py
@@ -7,3 +7,10 @@ def search_page():
     Loads the main page
     """
     return render_template('search-page.html')
+
+@app.route('/visualize')
+def visualize():
+    """
+    Loads the visualization page
+    """
+    return render_template('viz-page.html')
