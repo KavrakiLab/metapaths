fec7aa6cf7b2faacb4b675d6d88e1aac04c66b6d
Prudhvi Boyapalli <prrb02@gmail.com>
2017-02-22 16:36:27 -0600

    Added tentative routes for executing a search and loading results

--- metapaths.py
+++ metapaths.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+# Global dictionary for storing metapath search results
+# graph_id : results object
+graphs = {}
+
 @app.route('/')
 def search_page():
     """
@@ -8,9 +13,31 @@ def search_page():
     """
     return render_template('search-page.html')
 
+
 @app.route('/visualize')
 def visualize():
     """
     Loads the visualization page
     """
     return render_template('viz-page.html')
+
+@app.route('/load_graph/<graph_id>')
+def load_data(graph_id):
+    """
+    Looks up the graph associated with this graph_id and responds with a JSON
+    representation of the graph
+    """
+    # TODO: validate the JSON? Return error code if invaild
+    graph = graphs[graph_id]
+    # TODO: convert to JSON and send in the response
+
+
+@app.route('/search')
+def search():
+    """
+    Executes a user's configured search, stores the resulting graph locally and
+    responds with a graph_id which can be used to later vizualize the results
+    """
+    # TODO: Generate a graph_id
+    # TODO: Execute the search (async?)
+    # TODO: Return the graph_id in the reponse
