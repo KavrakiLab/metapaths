8f15915208449005b3747d04574684b51ff9491f
Prudhvi Boyapalli <prrb02@gmail.com>
2017-03-31 22:04:04 -0500

    Added ability to load previous search; Created initial tooltip when right-clicking on nodes

--- static/scripts/search-page.js
+++ static/scripts/search-page.js
@@ -5,3 +5,8 @@ $(function () {
 function upload() {
     location.assign("/visualize");
 }
+
+function load_search(search_id) {
+    localStorage.setItem("search_id", search_id);
+    location.assign("/visualize");
+}
