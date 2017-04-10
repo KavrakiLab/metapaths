05314eb10912d36e6a9d0887bff7a7444cfc166c
Prudhvi Boyapalli <prrb02@gmail.com>
2017-04-10 02:05:47 -0500

    Added retrieval of compounds names to search form

--- static/scripts/search-page.js
+++ static/scripts/search-page.js
@@ -1,5 +1,24 @@
 $(function () {
     $('[data-toggle="popover"]').popover({container: 'body'});
+
+    $.get("/get_compound_names").done(function(results_json) {
+        var compound_names = format_compounds(JSON.parse(results_json));
+
+        $('#start-compound').select2({
+            data: compound_names,
+            placeholder: "Select a start compound",
+            allowClear: true
+        });
+        $('#target-compound').select2({
+            data: compound_names,
+            placeholder: "Select a target compound",
+            allowClear: true
+        });
+    }).fail(function(error) {
+        console.log(error);
+        alert("An error occured, please refresh the page to try again.");
+    });
+
 });
 
 function upload() {
@@ -9,4 +28,18 @@ function upload() {
 function load_search(search_id) {
     localStorage.setItem("search_id", search_id);
     location.assign("/visualize");
+}
+
+function format_compounds(name_map) {
+    var formatted_names = [];
+
+    for (var id in name_map) {
+        formatted_names.push({
+            "id" : id,
+            "text" : name_map[id]
+        });
+    }
+
+    return formatted_names;
+    
 }
