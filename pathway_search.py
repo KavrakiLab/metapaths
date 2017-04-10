63ea0bc28c559422cedac1c9798d55a86eba4138
Prudhvi Boyapalli <prrb02@gmail.com>
2017-04-10 03:15:37 -0500

    Connected front end with back end

---
+++ pathway_search.py
@@ -0,0 +1,7 @@
+def execute_hub_search(start, target, num_atoms, allow_reversible):
+    """docstring for exe"""
+    print "Executing hub search with:"
+    print(start, target, num_atoms, allow_reversible)
+
+    return "testsearchid1234" # TODO: return real search id
+    
