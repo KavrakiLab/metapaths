45bc846bf44bcc2db10a330aba0b36baf9140f7e
Prudhvi Boyapalli <prrb02@gmail.com>
2017-04-11 02:38:24 -0500

    Implemented search config file generation for LPAT

--- pathway_search.py
+++ pathway_search.py
@@ -1,14 +1,47 @@
+import uuid
+
 def execute_hub_search(start, target, num_atoms, allow_reversible):
     """docstring for exe"""
     print "Executing hub search with:"
     print(start, target, num_atoms, allow_reversible)
+    search_id = str(uuid.uuid4()) # TODO: Is this okay to do?
 
-    return "testHUBresult1234" # TODO: return real search id
+    return search_id
 
 def execute_lpat_search(start, target, num_atoms, allow_reversible):
     """docstring for exe"""
     print "Executing LPAT search with:"
     print(start, target, num_atoms, allow_reversible)
+    search_id = str(uuid.uuid4()) # TODO: Is this okay to do?
+    config_loc = generate_LPAT_config(start, target, num_atoms, allow_reversible, search_id)
 
-    return "testLPATresult1234" # TODO: return real search id
+
+    return search_id
+
+
+def generate_LPAT_config(start, target, num_atoms, allow_reversible, search_id):
+    config = "DBHOST\tlocalhost\n"
+    config += "DBPORT\t3306\n"
+    config += "DBNAME\tmetadb\n"
+    config += "DBUSER\troot\n"
+    config += "DBPASS\tmeta\n"
+    config += "MOLDIR\trpair_mol\n"
+    config += "RPAIRDIR\trpair_only_maps\n"
+    config += "REACTIONDIR\trpair_maps\n"
+    config += "K\t1000000\n"
+    config += "USEREVERSE\t1\n"
+    config += "STARTCID\t" + start + "\n"
+    config += "TARGETCID\t" + target + "\n"
+    if num_atoms > 0:
+        config += "CARBONTRACK\t" + str(num_atoms) + "\n"
+    else:
+        config += "CARBONTRACK\tmax\n"
+    config += "PERCENTCARBON\t1.0\n"
+    config += "WEIGHTTYPE\tWEIGHT_OF_ONE\n"
+    config += "OUTPUTDIR\tLPAT_OUTPUT\n"
+
+    config_file = open("LPAT/LPAT_INPUT/" + search_id + ".txt", 'w')
+    config_file.write(config)
+    config_file.close()
+
 
