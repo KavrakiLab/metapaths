b4149b6609f99f1728a5549006b4017546f742fa
Prudhvi Boyapalli <prrb02@gmail.com>
2017-04-11 01:54:16 -0500

    Cleaned up form inputs

--- static/scripts/search-page.js
+++ static/scripts/search-page.js
@@ -1,6 +1,10 @@
 $(function () {
     $('[data-toggle="popover"]').popover({container: 'body'});
 
+    var alg_options = [{ id: 0, text: 'Hub Search' }, { id: 1, text: 'LPAT' }, { id: 2, text: 'MetaGraph' }];
+
+    $('#algorithm').select2({data: alg_options});
+
     $.get("/get_compound_names").done(function(results_json) {
         var compound_names = format_compounds(JSON.parse(results_json));
 
@@ -9,11 +13,17 @@ $(function () {
             placeholder: "Select a start compound",
             allowClear: true
         });
+        // Set an initial suggested value
+        $('#start-compound').select2().val("C00668").trigger("change");
+
         $('#target-compound').select2({
             data: compound_names,
             placeholder: "Select a target compound",
             allowClear: true
         });
+        // Set an initial suggested value
+        $('#target-compound').select2().val("C01613").trigger("change");
+
     }).fail(function(error) {
         console.log(error);
         alert("An error occured, please refresh the page to try again.");
@@ -36,7 +46,7 @@ function format_compounds(name_map) {
     for (var id in name_map) {
         formatted_names.push({
             "id" : id,
-            "text" : name_map[id]
+            "text" : name_map[id] + " (" + id + ")"
         });
     }
 
@@ -46,7 +56,8 @@ function format_compounds(name_map) {
 
 function search() {
 
-    if ($("#algorithm").val() === "Hub Search") {
+    // If user selected hub search
+    if ($("#algorithm").val() === "0") {
         var query = {
             "start" : $("#start-compound").val(),
             "target" : $("#target-compound").val(),
@@ -74,5 +85,9 @@ function search() {
 
             }
         });
+    } else if ($("#algorithm").val() === "1") {
+    } else if ($("#algorithm").val() === "2") {
+    } else {
+        alert("Invalid algorithm selection.");
     }
 }
