aa43d71c103cc1b89960c1d26dca749905736210
Prudhvi Boyapalli <prrb02@gmail.com>
2017-04-13 02:08:55 -0500

    Split compound and organism search into two fields

--- static/scripts/highlighting.js
+++ static/scripts/highlighting.js
@@ -1,22 +1,19 @@
 
 function initialize_search_panel() {
-    // TODO: should we fetch nicely formatted results from the server?
-    $('select')[0].innerHTML = generate_dropdown_data();
-
-
     // Enable select menu
-    $('select').select2({
-        placeholder: "Start type or select an item",
+    $('#compounds-dropdown').select2({
+        data: get_compounds_list(),
+        placeholder: "Start typing or click to select an item",
         allowClear: true
     });
 
-    $('select').on("select2:select", function(e){
-        var selected_val = $('select').select2().val();
+    $('#compounds-dropdown').on("select2:select", function(e){
+        var selected_val = $('#compounds-dropdown').select2().val();
         highlight(selected_val);
     });
 
-    $('select').on("select2:selecting", function(e){
-        var unselected_val = $('select').select2().val();
+    $('#compounds-dropdown').on("select2:selecting", function(e){
+        var unselected_val = $('#compounds-dropdown').select2().val();
         if (unselected_val != null) {
             remove_highlight(unselected_val);
         }
@@ -45,17 +42,14 @@ function remove_highlight(hl_element) {
     }
 }
 
-function generate_dropdown_data() {
-    var options_html  = "<optgroup label='Compounds'>";
+function get_compounds_list() {
+    var items = []
 
     d3.selectAll("circle").data().forEach(function(node) {
-        options_html += "<option value='" + node.id + "'>" + kegg_data[node.id].name + "</option>";
+        items.push({"id" : node.id, "text" : kegg_data[node.id].name})
     });
-    options_html += "</optgroup><optgroup label='Organisms'>";
-    options_html += "<option value='test-organism'>Test Organism</option>";
-    options_html += "</optgroup>";
 
-    return options_html;
+    return items;
 }
 
 
