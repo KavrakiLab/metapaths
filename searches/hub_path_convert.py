fef2b4b6833c122cbc0f6a9818833e8cfb518301
samikim <skim2105@gmail.com>
2018-05-09 02:33:36 -0500

    Revamped how hub paths are presented

--- searches/hub_path_convert.py
+++ searches/hub_path_convert.py
@@ -11,11 +11,6 @@ def convert_lpat(filename):
     f.seek(0)
     f.truncate()
 
-    #print(lines)
-    first_path_list = []
-    hub_path_list = []
-    second_path_list = []
-
     for less_than_two_hub_paths in split_content[1:-1]:
         if len(less_than_two_hub_paths) > 0:
             path_lines = less_than_two_hub_paths.split("\n")
@@ -40,9 +35,14 @@ def convert_lpat(filename):
 
     # Convert each path and write out to the same file
     lines = split_content[-1].split("\n")
+    #print(lines)
+    first_path_list = {}
+    hub_path_list = {}
+    second_path_list = {}
+    
     first_path = True
     for line in lines:
-        print line
+        #print line
         tab_split_line = line.split("\t")
         if("[" in line and first_path):
             path = ""
@@ -56,8 +56,10 @@ def convert_lpat(filename):
             path = path[:-1] + "_HS,"
 
             if len(tab_split_line) == 3:
-                path += "\t" + tab_split_line[2]
-            first_path_list.append(path)
+                c_conserved = tab_split_line[2]
+                if c_conserved not in first_path_list:
+                    first_path_list[c_conserved] = []
+                first_path_list[c_conserved].append(path)
 
         elif("[" not in line):
             first_path = False
@@ -67,8 +69,10 @@ def convert_lpat(filename):
                 path += item[0:6] + ","
 
             if len(tab_split_line) == 2:
-                path += "\t" + tab_split_line[1]
-            hub_path_list.append(path)
+                c_conserved = tab_split_line[1]
+                if c_conserved not in hub_path_list:
+                    hub_path_list[c_conserved] = []
+                hub_path_list[c_conserved].append(path)
 
         elif("[" in line):
             raw_path = line.split(";")
@@ -84,30 +88,72 @@ def convert_lpat(filename):
                         path += cleaned_item[0:7] + ","
 
             if len(tab_split_line) == 3:
-                path += "\t" + tab_split_line[2]
-            second_path_list.append(path)
+                c_conserved = tab_split_line[2]
+                if c_conserved not in second_path_list:
+                    second_path_list[c_conserved] = []
+                second_path_list[c_conserved].append(path)
 
+    if len(first_path_list) == 0 and len(second_path_list) == 0:
+        for cc in hub_path_list:
+            for path in hub_path_list[cc]:
+                f.write(path[:-1] + "\t" + cc + "\n")
 
-    min_length = len(first_path_list)
-    if(min_length > len(second_path_list)):
-        min_length = len(second_path_list)
-    for i in range(min_length):
-        hub_middle = hub_path_list[0]
-        if(len(hub_path_list) > i):
-            hub_middle = hub_path_list[i]
+    elif len(first_path_list) == 0:
+        for cc1 in hub_path_list:
+            for cc2 in second_path_list:
+                hub_1, hub_2 = get_carbon_conserved_arrays(cc1)
+                second_1, second_2 = get_carbon_conserved_arrays(cc2)
+                final_1, final_2 = get_merged_carbons_conserved(hub_1, hub_2, second_1, second_2)
+                if len(final_1) > 0:
+                    cc_str = get_str_cc(final_1, final_2)
+                    for path1 in hub_path_list[cc1]:
+                        for path2 in second_path_list[cc2]:
+                            f.write(path1 + path2[:-1] + "\t" + cc_str + "\n")
 
-        first_path_info = first_path_list[i].split("\t")
-        hub_middle_info = hub_middle.split("\t")
-        second_path_info = second_path_list.split("\t")
-        carbons_conserved = ""
+    elif len(second_path_list) == 0:
+         for cc1 in first_path_list:
+            for cc2 in hub_path_list:
+                first_1, first_2 = get_carbon_conserved_arrays(cc1)
+                hub_1, hub_2 = get_carbon_conserved_arrays(cc2)
+                final_1, final_2 = get_merged_carbons_conserved(first_1, first_2, hub_1, hub_2)
+                if len(final_1) > 0:
+                    cc_str = get_str_cc(final_1, final_2)
+                    for path1 in first_path_list[cc1]:
+                        for path2 in hub_path_list[cc2]:
+                            f.write(path1 + path2[:-1] + "\t" + cc_str + "\n")
 
-        if len(first_path_info) > 1 and len(hub_middle_info) > 1 and len(second_path_info) > 1:
-            carbons_conserved = get_carbons_conserved(first_path_info[1], hub_middle_info[1], second_path_info[1])
+    else:
+        for cc1 in first_path_list:
+            for cc2 in hub_path_list:
+                for cc3 in second_path_list:
+                        cc_str = get_carbons_conserved(cc1, cc2, cc3)
+                    if len(cc_str) > 0:
+                        for path1 in first_path_list[cc1]:
+                            for path2 in hub_path_list[cc2]:
+                                for path3 in second_path_list[cc3]:
+                                    f.write(path1 + path2 + path3[:-1] + "\t" + cc_str + "\n")        
 
-        f.write(first_path_info[0] + hub_middle_info[0] + second_path_info[0])
-        f.seek(-1, os.SEEK_CUR)
-        f.write("\t" + carbons_conserved + "\n")
     f.close()
+
+    # min_length = len(first_path_list)
+    # if(min_length > len(second_path_list)):
+    #     min_length = len(second_path_list)
+    # if(min_length != 0):
+
+    # for i in range(min_length):
+    #     hub_middle = hub_path_list[0]
+    #     if(len(hub_path_list) > i):
+    #         hub_middle = hub_path_list[i]
+
+    #     first_path_info = first_path_list[i].split("\t")
+    #     hub_middle_info = hub_middle.split("\t")
+    #     second_path_info = second_path_list.split("\t")
+    #     carbons_conserved = ""
+
+    #     if len(first_path_info) > 1 and len(hub_middle_info) > 1 and len(second_path_info) > 1:
+    #         carbons_conserved = get_carbons_conserved(first_path_info[1], hub_middle_info[1], second_path_info[1])
+    #         f.write(first_path_info[0] + hub_middle_info[0] + second_path_info[0])
+
 
 def get_carbon_conserved_arrays(carbon_conserved_string):
     cc_list = re.findall(carbon_conserved_string,"[0-9]+:[0-9]+")
@@ -140,6 +186,10 @@ def get_carbons_conserved(first_path_cc, hub_cc, second_path_cc):
 
     final_1, final_2 = get_merged_carbons_conserved(temp_1, temp_2, final_1, final_2)
 
+    return get_str_cc(final_1, final_2)
+
+
+def get_str_cc(final_1, final_2):
     final_str = ""
     for i in range(len(final_1)):
         final_str += final_1[i] + ":" + final_2[i]
