36246c8f6dcb21a18b0001aa896edb3a061b2c16
samikim <skim2105@gmail.com>
2018-05-08 19:57:00 -0500

    Updated conversion script for hub paths to include carbons conserved

--- searches/hub_path_convert.py
+++ searches/hub_path_convert.py
@@ -20,6 +20,7 @@ def convert_lpat(filename):
     first_path = True
     for line in lines:
         #print(line)
+        tab_split_line = line.split("\t")
         if("[" in line and first_path):
             path = ""
             for item in line.split(";"):
@@ -30,6 +31,9 @@ def convert_lpat(filename):
                     elif cleaned_item[0] == "R":
                         path += cleaned_item[0:7] + ","
             path = path[:-1] + "_HS,"
+
+            if len(tab_split_line) == 3:
+                path += "\t" + tab_split_line[2]
             first_path_list.append(path)
 
         elif("[" not in line):
@@ -38,6 +42,9 @@ def convert_lpat(filename):
             path = ""
             for item in raw_path[1:-1]:
                 path += item[0:6] + ","
+
+            if len(tab_split_line) == 2:
+                path += "\t" + tab_split_line[1]
             hub_path_list.append(path)
 
         elif("[" in line):
@@ -52,6 +59,9 @@ def convert_lpat(filename):
                         path += cleaned_item[0:6] + ","
                     elif cleaned_item[0] == "R":
                         path += cleaned_item[0:7] + ","
+
+            if len(tab_split_line) == 3:
+                path += "\t" + tab_split_line[2]
             second_path_list.append(path)
 
 
@@ -63,10 +73,54 @@ def convert_lpat(filename):
         if(len(hub_path_list) > i):
             hub_middle = hub_path_list[i]
 
-        f.write(first_path_list[i] + hub_middle + second_path_list[i])
+        first_path_info = first_path_list[i].split("\t")
+        hub_middle_info = hub_middle.split("\t")
+        second_path_info = second_path_list.split("\t")
+        carbons_conserved = ""
+
+        if len(first_path_info) > 1 and len(hub_middle_info) > 1 and len(second_path_info) > 1:
+            carbons_conserved = get_carbons_conserved(first_path_info[1], hub_middle_info[1], second_path_info[1])
+
+        f.write(first_path_info[0] + hub_middle_info[0] + second_path_info[0] + "\t" + carbons_conserved)
         f.seek(-1, os.SEEK_CUR)
         f.write("\n")
     f.close()
+
+def get_carbon_conserved_arrays(carbon_conserved_string):
+    cc_list = re.findall(carbon_conserved_string,"[0-9]+:[0-9]+")
+    first_list = []
+    second_list = []
+    for item in cc_list:
+        ccs = item.split(":")
+        first_list.append(ccs[1])
+        second_list.append(ccs[2])
+
+    return first_list, second_list
+
+def get_merged_carbons_conserved(first_1, first_2, second_1, second_2):
+    intersection = set(first_2) & set(second_1)
+
+    new_mapping_1 = []
+    new_mapping_2 = []
+
+    for item in intersection:
+        new_mapping_1.append(first_1[first_2.index(item)])
+        new_mapping_2.append(second_2[second_1.index(item)])
+
+    return new_mapping_1, new_mapping_2
+
+def get_carbons_conserved(first_path_cc, hub_cc, second_path_cc):
+    first_1, first_2 = get_carbon_conserved_arrays(first_path_cc)
+    middle_1, middle_2 = get_carbon_conserved_arrays(hub_cc)
+    last_1, last_2 = get_carbon_conserved_arrays(second_path_cc)
+    temp_1, temp_2 = get_merged_carbons_conserved(first_1, first_2, middle_1, middle_2)
+
+    final_1, final_2 = get_merged_carbons_conserved(temp_1, temp_2, final_1, final_2)
+
+    final_str = ""
+    for i in range(len(final_1)):
+        final_str += final_1[i] + ":" + final_2[i]
+    return final_str
 
 
 convert_lpat(sys.argv[1])
