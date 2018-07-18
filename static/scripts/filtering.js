74129c0c48c1e3e4fded114d715e4271e610bdd9
sarahkim <sarahkim@rice.edu>
2018-07-17 22:15:12 -0500

    Changed read in

--- static/scripts/filtering.js
+++ static/scripts/filtering.js
@@ -54,6 +54,7 @@ function filter_pathways(str_info, str_pathways, background_hubs) {
     filtered_pathways["info"] = JSON.parse(str_info);
     filtered_pathways["pathways"] = JSON.parse(str_pathways);
     filtered_pathways["background_hubs"] = background_hubs;
+    console.log(background_hubs);
 
     // First pass: remove pathways which contain an excluded compound
     if ($("#excluded-ids").val() !== "") {
